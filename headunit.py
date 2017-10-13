9de0c415f1fae29e36480b14f74869321f7f2bf3
Sjoerd82 <srg.venema@gmail.com>
2017-10-13 17:35:29 +0200

    1735

--- headunit.py
+++ headunit.py
@@ -554,7 +554,12 @@ def usb_check():
 	# So, let's check if there's anything in the database for this source:
 	
 	if arSourceAvailable[1] == 1:
-		print(' ... /media has mounted filesystems... Continuing to check if there''s music...')	
+		print(' ... /media has mounted filesystems... continuing to check mpd database for music...')	
+
+		mountpoint = subprocess.check_output("mount | egrep media | cut -d ' ' -f 3", shell=True)
+		sUsbLabel = os.path.basename(mountpoint)
+		print(' ... label of this filesystem: {0:s}'.format(sUsbLabel))
+		
 		#taskcmd = "mpc listall "+sUsbLabel+" | wc -l" #TODO - FIX
 		taskcmd = "mpc listall SJOERD | wc -l"
 		task = subprocess.Popen(taskcmd, shell=True, stdout=subprocess.PIPE)
@@ -739,18 +744,18 @@ def source_check():
 	linein_check()
 
 	# Display source availability.
-	print('- Summary ------------------------')
+	print('-- Summary ------------------------')
 	print('Current source: {0:d}'.format(dSettings['source']))
 	
 	i = 0
 	for source in arSource:
 		if arSourceAvailable[i] == 1:
-			print('{0:d} {1:6}\033[92mavailable \033[00m'.format(i,source))
+			print(' {0:d} {1:8}\033[92mavailable \033[00m'.format(i,source))
 		else:
-			print('{0:d} {1:6}\033[91mnot available \033[00m'.format(i,source))
+			print(' {0:d} {1:8}\033[91mnot available \033[00m'.format(i,source))
 		i += 1
 	
-	print('----------------------------------')
+	print('-----------------------------------')
 
 def source_next():
 	global dSettings
