616fe5fb707119a0bbf2f4d80f0f93d4e792151e
Sjoerd82 <srg.venema@gmail.com>
2017-10-13 19:53:53 +0200

    1953

--- hu_usb_removed.sh
+++ hu_usb_removed.sh
@@ -7,11 +7,13 @@ typeset params_mpc=""
 typeset root_folder=""
 
 # todo test parameter is there..
+folder_x='/root/defender-headunit'
 root_folder=$(basename $1)
 
+
 # save position and current file name for this drive
-mpc | sed -n 2p | grep -Po '(?<=#)[^/]*' > /home/hu/mp_$root_folder.txt
-mpc -f %file% current >> /home/hu/mp_$root_folder.txt
+mpc | sed -n 2p | grep -Po '(?<=#)[^/]*' > $folder_x/mp_$root_folder.txt
+mpc -f %file% current >> $folder_x/mp_$root_folder.txt
 
 mpc $params_mpc -q stop
 mpc $params_mpc -q clear
