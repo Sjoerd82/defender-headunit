1dd4d98c5adc599d1351278a9aa9788d626f6bed
Sjoerd82 <srg.venema@gmail.com>
2017-10-29 10:56:03 +0100

    1056

--- remote.py
+++ remote.py
@@ -3,45 +3,81 @@ import random
 import time
 
 import threading
-class SlowThread(object):
-    def __init__(self, bits, callback):
-        self._callback = callback
-        self.result = ''
 
-        self.thread = threading.Thread(target=self.work, args=(bits,))
-        self.thread.start()
-        self.thread_id = str(self.thread.ident)
+# Import the ADS1x15 module.
+import Adafruit_ADS1x15
 
-    def work(self, bits):
-        num = ''
+class RemoteControl(dbus.service.Object):
+	# ADC remote variables
+	GAIN = 2/3
+	BUTTON01_LO = 180
+	BUTTON01_HI = 190
+	BUTTON02_LO = 220
+	BUTTON02_HI = 260
+	BUTTON03_LO = 310
+	BUTTON03_HI = 330
+	BUTTON04_LO = 380
+	BUTTON04_HI = 410
+	BUTTON05_LO = 460
+	BUTTON05_HI = 490
+	BUTTON06_LO = 560
+	BUTTON06_HI = 580
+	BUTTON07_LO = 640
+	BUTTON07_HI = 670
+	BUTTON08_LO = 740
+	BUTTON08_HI = 770
+	BUTTON09_LO = 890
+	BUTTON09_HI = 910
+	BUTTON10_LO = 1050
+	BUTTON10_HI = 1100
 
-        while True:
-            num += str(random.randint(0, 1))
-            bits -= 1
-            time.sleep(1)
+	def __init__(self, bus_name):
+		super(RemoteControl,self).__init__(bus_name, "/com/arctura/remote")
+		adc = Adafruit_ADS1x15.ADS1015()
 
-            if bits <= 0:
-                break
+		while True:
+			value_0 = adc.read_adc(0, gain=self.GAIN)
+			value_1 = adc.read_adc(1, gain=self.GAIN)
+			if self.BUTTON01_LO <= value_0 <= self.BUTTON01_HI:
+				#Bottom button
+				self.button_press('UPDATE_LOCAL')
 
-        self._callback(self.thread_id, str(int(num, 2)))
+			elif self.BUTTON02_LO <= value_0 <= self.BUTTON02_HI:
+				#Side button, met streepje
+				print('BUTTON02')
 
-class RandomData(dbus.service.Object):
-    def __init__(self, bus_name):
-        super(RandomData,self).__init__(bus_name, "/com/larry_price/test/RandomData")
+			elif self.BUTTON03_LO <= value_0 <= self.BUTTON03_HI:
+				self.button_press('VOL_UP')
+				
+			elif self.BUTTON04_LO <= value_0 <= self.BUTTON04_HI:
+				self.button_press('VOL_DOWN')
+				
+			elif self.BUTTON05_LO <= value_0 <= self.BUTTON05_HI:
+				if value_1 < 300:
+					self.button_press('SEEK_NEXT')
+				else:
+					self.button_press('DIR_NEXT')
 
-        random.seed()
+			elif self.BUTTON06_LO <= value_0 <= self.BUTTON06_HI:
+				if value_1 < 300:
+					self.button_press('SEEK_PREV')
+				else:
+					self.button_press('DIR_PREV')
 
-    @dbus.service.method("com.larry_price.test.RandomData",
-                         in_signature='i', out_signature='s')
-    def quick(self, bits=8):
-        return str(random.getrandbits(bits))
+			elif self.BUTTON07_LO <= value_0 <= self.BUTTON07_HI:
+				self.button_press('SHUFFLE')
 
-    @dbus.service.method("com.larry_price.test.RandomData",
-                         in_signature='i', out_signature='s')
-    def slow(self, bits=8):
-        thread = SlowThread(bits, self.slow_result)
-        return thread.thread_id
+			elif self.BUTTON08_LO <= value_0 <= self.BUTTON08_HI:
+				self.button_press('ATT')
 
-    @dbus.service.signal("com.larry_price.test.RandomData", signature='ss')
-    def slow_result(self, thread_id, result):
-        pass
+			elif self.BUTTON09_LO <= value_0 <= self.BUTTON09_HI:
+				self.button_press('SOURCE')
+
+			elif self.BUTTON10_LO <= value_0 <= self.BUTTON10_HI:
+				self.button_press('OFF')
+				
+			time.sleep(0.1)
+
+	@dbus.service.signal("com.arctura.remote", signature='s')
+	def button_press(self, button):
+		print("Button was pressed")
--- remote_dbus.py
+++ remote_dbus.py
@@ -6,53 +6,32 @@
 import dbus, dbus.service, dbus.exceptions
 import sys
 
-class RemoteControl(dbus.service.Object):
-	def __init__(self):
-		#self.session_bus = dbus.SessionBus()
-		#name = dbus.service.BusName("com.example.SampleService", bus=self.session_bus)
-		#dbus.service.Object.__init__(self, name, '/SomeObject')
-		# Declare a name where our service can be reached
-		try:
-			bus_name = dbus.service.BusName("com.arctura.remote",
-											bus=dbus.SystemBus(),
-											do_not_queue=True)
-		except dbus.exceptions.NameExistsException:
-			print("service is already running")
-			sys.exit(1)
-    @dbus.service.method("com.example.SampleInterface", in_signature='s', out_signature='as')
-    def HelloWorld(self, hello_message):
-        return ["Hello", "from example-service.py", "with unique name", self.session_bus.get_unique_name()]
-    @dbus.service.method("com.example.SampleInterface", in_signature='', out_signature='')
-    def Exit(self):
-        mainloop.quit()
-		
-if __name__ == '__main__':
-	# using gobject
-	#from dbus.mainloop.glib import DBusGMainLoop
-	import dbus.mainloop.glib
-	import gobject
-	
-	# Initialize a main loop
-	DBusGMainLoop(set_as_default=True)
-	loop = gobject.MainLoop()
-	
-	object = RemoteControl()
-	#loop.run()
+from dbus.mainloop.glib import DBusGMainLoop
+import gobject
 
-	# Run the loop
-	try:
-		# Create our initial objects
-		#from dbustest.random_data import RandomData
-		#RandomData(bus_name)
-		#from random_data import RemoteControl
-		#RemoteControl(bus_name)
-		
-		loop.run()
-	except KeyboardInterrupt:
-		print("keyboard interrupt received")
-	except Exception as e:
-		print("Unexpected exception occurred: '{}'".format(str(e)))
-	finally:
-		#loop.quit()
+# Initialize a main loop
+DBusGMainLoop(set_as_default=True)
+loop = gobject.MainLoop()
 
-	
+# Declare a name where our service can be reached
+try:
+    bus_name = dbus.service.BusName("com.arctura.remote",
+                                    bus=dbus.SystemBus(),
+                                    do_not_queue=True)
+except dbus.exceptions.NameExistsException:
+    print("service is already running")
+    sys.exit(1)
+
+# Run the loop
+try:
+    # Create our initial objects
+    from random_data import RemoteControl
+    RemoteControl(bus_name)
+    loop.run()
+except KeyboardInterrupt:
+    print("keyboard interrupt received")
+except Exception as e:
+    print("Unexpected exception occurred: '{}'".format(str(e)))
+finally:
+    print("quitting...")
+    loop.quit()
