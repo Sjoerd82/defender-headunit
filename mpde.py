1484b75949e0937dbdc7d2ff2837fe43b801b939
Sjoerd82 <srg.venema@gmail.com>
2017-12-05 15:47:12 +0100

    1547

--- mpde.py
+++ mpde.py
@@ -40,20 +40,62 @@ class mpdControl(dbus.service.Object):
 			canRead = select([self.oMpdClient], [], [], 0)[0]
 			if canRead:
 				changes = self.oMpdClient.fetch_idle()
-				#self.mpd_handle_change(changes)
-				self.mpd_control(changes)
-				
-				self.oMpdClient.command_list_ok_begin()
-				self.oMpdClient.readmessages()
-				messages = self.oMpdClient.command_list_end()
-				print messages
+				self.mpd_handle_change(changes)
+				#self.mpd_control(changes)
+			
+#				self.oMpdClient.command_list_ok_begin()
+#				self.oMpdClient.readmessages()
+#				messages = self.oMpdClient.command_list_end()
+#				print messages
 
 				self.oMpdClient.send_idle() # continue idling
 			
 			time.sleep(0.1)
 
+	def mpd_handle_change(events):
+	
+		for e in events:
+
+			print(' ...  EVENT: {0}'.format(e))
+			if e == "message":	
+				#oMpdClient.subscribe("media_ready")
+				#oMpdClient.command_list_ok_begin()
+				#oMpdClient.readmessages()
+				#messages = oMpdClient.command_list_end()
+				#for m in messages:
+				#	print(' ...  MESSAGE: {0}'.format(m))
+				
+				self.oMpdClient.command_list_ok_begin()
+				self.oMpdClient.readmessages()
+				messages = self.oMpdClient.command_list_end()
+				print messages
+				
+			elif e == "player" or 'mixer':
+				#oMpdClient.command_list_ok_begin()
+				#oMpdClient.status()
+				#results = oMpdClient.command_list_end()		
+				#
+				#for r in results:
+				#	print(r)
+				self.mpd_control('player_mixer')
+				
+			#elif e == "subscription":
+			#	oMpdClient.command_list_ok_begin()
+			#	oMpdClient.channels()
+			#	results = oMpdClient.command_list_end()		
+			#
+			#	for r in results:
+			#		print(r)		
+
+			else:
+				print(' ...  unmanaged event')
+	
 	#oMpdClient will create a list variable with the change events
-	@dbus.service.signal("com.arctura.mpd", signature='as')
-	def mpd_control(self, event):
-		print(event)
+	#@dbus.service.signal("com.arctura.mpd", signature='as')
+	#def mpd_control(self, event):
+	#	print(event)
+		
+	@dbus.service.signal("com.arctura.mpd", signature='s')
+	def mpd_control(self, ding)
+		print
 	
