05fe07a68b08c354d41b7274c6ec2403613c99ca
Sjoerd82 <srg.venema@gmail.com>
2018-01-22 20:00:26 +0100

    2000

--- test.py
+++ test.py
@@ -229,8 +229,10 @@ def cb_mpd_event( event ):
 				mpc_save_pos_for_label( currSrc['name'], "/mnt/PIHU_CONFIG" )
 
 		#hu_details
-		print mpdc.mpc_get_currentsong()
-		print mpdc.mpc_get_status()
+		#print mpdc.mpc_get_currentsong()
+		mpcStatus = mpdc.mpc_get_status()
+		disp.lcd_play( mpcStatus['artist'], mpcStatus['title'], mpcStatus['track'] )
+		
 				
 	elif event == "update":
 		printer(" ...  database update started or finished (no action)", tag='MPD')
