8bad4f650f1f0a87898e2820b03d09685e47564e
Sjoerd82 <srg.venema@gmail.com>
2018-01-25 19:48:13 +0100

    1948

--- sources/media.py
+++ sources/media.py
@@ -42,7 +42,7 @@ def media_init( sourceCtrl ):
 	# add all locations as configured
 	arMedia = media_getAll()
 	for dev_mp in arMedia:
-		if not devmp[1] == 'PIHU_DATA' and not devmp[0].startswith('//'):
+		if not dev_mp[1] == 'PIHU_DATA' and not dev_mp[0].startswith('//'):
 			mountpoint = dev_mp[1]
 			sUsbLabel = os.path.basename(dev_mp[1]).rstrip('\n')
 			#TODO: try-except for subprocess?
