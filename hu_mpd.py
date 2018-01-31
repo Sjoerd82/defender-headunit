aca4a38cddfc890cbd8845ba758da1e9308c81dd
Sjoerd82 <srg.venema@gmail.com>
2018-01-31 23:30:48 +0100

    2330

--- hu_mpd.py
+++ hu_mpd.py
@@ -28,12 +28,13 @@ sDirSave = "/mnt/PIHU_CONFIG"
 
 class mpdController():
 
-	self.mpdc = MPDClient()		# class attribute -- shared by all instances... gives irrelevant connect errors... not sure if this is good or bad
+	#self.mpdc = MPDClient()		# class attribute -- shared by all instances... gives irrelevant connect errors... not sure if this is good or bad
 
 	def __init__( self ):
 		# Connect to MPD
 		try:
 			printer('Initializing MPD client')
+			self.mpdc = MPDClient()
 			self.mpdc.timeout = 10                # network timeout in seconds (floats allowed), default: None
 			self.mpdc.idletimeout = None          # timeout for fetching the result of the idle command is handled seperately, default: None
 			self.mpdc.connect("localhost", 6600)
