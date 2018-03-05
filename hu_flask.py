3f4e81f9d6048b85ddc0dfb5cff2bc440d1d15db
Sjoerd82 <srg.venema@gmail.com>
2018-03-05 21:22:52 +0100

    2122

--- hu_flask.py
+++ hu_flask.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python
 
 from flask import Flask
 from flask import render_template
@@ -8,7 +9,8 @@ import zmq
 
 app = Flask(__name__)
 
-
+ctx = zmq.Context()
+pub = ctx.socket(zmq.PUB)
 
 @app.route('/')
 def hello_world():
@@ -123,4 +125,38 @@ def main():
 	#	s.send_pyobj(a)
 	#print("   Done.")
 	
-main()
+#main()
+
+
+def publish_message(message):
+	url = "tcp://172.16.8.192:5555"
+	try:
+		pub.bind(url)
+		time.sleep(1)
+		print("sending message : {0}".format(message, pub))
+		pub.send(message)
+	except Exception as e:
+		print("error {0}".format(e))
+	finally:
+	# You wanna unbind the publisher to keep
+	# receiving the published messages
+	# Otherwise you would get a — Adress already in use — error
+	pub.unbind(url)
+
+
+# This is an endpoint which prints the
+# number we want to print in response
+# and also publishes a message containing the number
+@app.route("/print/", methods = ['GET'])
+def printNumber(number):
+	response = 'Number %d' % number
+	publish_message('number%d' % number)
+	return response
+
+# In python “__name__” will be
+# “__main__” whenever the script
+# file itself is called instead
+# of being used as a library
+if __name__ == ‘__main__’:
+	# The default port it will run on here is 5000
+	app.run(host=’0.0.0.0′, debug=True)
