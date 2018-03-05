3ac4f91dce014e927322647c908f0d91ce37e2e1
Sjoerd82 <srg.venema@gmail.com>
2018-03-05 19:57:03 +0100

    1956

--- hu_flask.py
+++ hu_flask.py
@@ -4,9 +4,36 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+	pages = [
+	  {"title":"State/Control","id":"state"}
+	 ,{"title":"Playlist","id":"playlist"}
+	 ,{"title":"Config","id":"config"}
+	 ,{"title":"API","id":"api"}
+	 ,{"title":"Logs","id":"log"}
+	]
+	return render_template('index.html', pages=pages)
 	
+@app.route('/api')
+def list_routes():
+	import urllib
+	output = []
+	for rule in app.url_map.iter_rules():
+
+		options = {}
+		for arg in rule.arguments:
+			options[arg] = "[{0}]".format(arg)
+
+		methods = ','.join(rule.methods)
+		url = url_for(rule.endpoint, **options)
+		line = urllib.unquote("{:50s} {:20s} {}".format(rule.endpoint, methods, url))
+		output.append(line)
+	
+	for line in sorted(output):
+		print line
+
 @app.route('/hello/')
 @app.route('/hello/<name>')
 def hello(name=None):
 	return render_template('hello.html', name=name)
+	
+	
