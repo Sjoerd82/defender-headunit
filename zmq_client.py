80f3764223f6072fd9ddb316c73fa8bf884df3a7
Sjoerd82 <srg.venema@gmail.com>
2018-03-06 18:40:59 +0100

    1840

--- zmq_client.py
+++ zmq_client.py
@@ -7,7 +7,7 @@ context = zmq.Context()
 
 subscriber = context.socket (zmq.SUB)
 subscriber.connect ("tcp://localhost:5555")
-subscriber.setsockopt (zmq.SUBSCRIBE, "")
+subscriber.setsockopt (zmq.SUBSCRIBE, "test|")
 
 print zmq.pyzmq_version()
 
