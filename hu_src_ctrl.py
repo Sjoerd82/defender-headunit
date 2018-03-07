7cfa564003dbbcb5f8c0ef0def5b33bf2b9db391
Sjoerd82 <srg.venema@gmail.com>
2018-03-07 20:15:14 +0100

    2015

--- hu_src_ctrl.py
+++ hu_src_ctrl.py
@@ -840,7 +840,7 @@ def parse_message(message):
 	cmd_par = path_cmd[1].split(":")
 
 	if len(cmd_par) == 1:
-		command = path_cmd
+		command = path_par
 	elif len(cmd_par) == 2:
 		command = cmd_par[0]
 		param = cmd_par[1]
@@ -852,7 +852,7 @@ def parse_message(message):
 	else:
 		print("Malformed message!")
 	
-	print("[MQ] Received Path: {0}, Command: {1}, Parameters: {2}".format(path,command,params))
+	print("[MQ] Received Path: {0}; Command: {1}; Parameters: {2}".format(path,command,params))
 
 	
 	
