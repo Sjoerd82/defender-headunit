7844bd34d50a39386defd73351b7490d5169ac0b
Sjoerd82 <srg.venema@gmail.com>
2018-03-08 08:07:31 +0000

    Delete manstart.sh

--- manstart.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
- #python /mnt/PIHU_APP/defender-headunit/plugins/output/dbus_2606a.py
- python /mnt/PIHU_APP/defender-headunit/plugins/control/dbus_ads1x15.py
- python /mnt/PIHU_APP/defender-headunit/dbus_mpd.py
