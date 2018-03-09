e20c0f527de77958cf1eb06039b97bba02e9db1f
Sjoerd82 <srg.venema@gmail.com>
2018-03-09 17:53:45 +0100

    1753

--- modules/hu_utils.py
+++ modules/hu_utils.py
@@ -36,10 +36,11 @@ LL_DEBUG = 10
 LL_NOTSET = 0
 
 # Defines how to handle output
-def myprint( message, level=LL_INFO, tag=""):
+def printer( message, level=LL_INFO, tag=""):
 	logger = logging.getLogger('headunit')
 	logger.log(level, message, extra={'tag': tag})
 
+	
 # ********************************************************************************
 # Load JSON configuration
 #
