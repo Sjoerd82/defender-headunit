001c8e5383cd3814cad7bc16e9dfdc7ebf8c3f00
Sjoerd82 <srg.venema@gmail.com>
2018-03-11 16:37:04 +0100

    1637

--- daemons/control_ads1x15.py
+++ daemons/control_ads1x15.py
@@ -85,12 +85,6 @@ def main():
 	GAIN = 2/3
 	BUTTON_LO   = 100
 	
-	BUTTON06_LO = 560	# 
-	BUTTON06_HI = 580	
-	
-	BUTTON07_LO = 640	# 
-	BUTTON07_HI = 670	
-	
 	BUTTON08_LO = 740	# "ATT"
 	BUTTON08_HI = 770	
 	
@@ -109,7 +103,7 @@ def main():
 	 , "channel0_hi": 190	# Starts at around 185, but then spikes to around 278
 	 , "wait"       : True	# Wait until button is released (no need to continue updating...)
 	 , "delay"      : None
-	 , "zmq_path"   : "\player\update"
+	 , "zmq_path"   : "/player/update"
 	 , "zmq_cmd"    : "SET" } )
 
 	# Side button, with raised center line (not assigned any task)
@@ -125,7 +119,7 @@ def main():
 	 , "channel0_hi": 330
 	 , "wait"       : False
 	 , "delay"      : None
-	 , "zmq_path"   : "\volume\increase"
+	 , "zmq_path"   : "/volume/increase"
 	 , "zmq_cmd"    : "SET" } )
 
 	# Volume DOWN
@@ -134,10 +128,10 @@ def main():
 	 , "channel0_hi": 410
 	 , "wait"       : False
 	 , "delay"      : None
-	 , "zmq_path"   : "\volume\decrease"
+	 , "zmq_path"   : "/volume/decrease"
 	 , "zmq_cmd"    : "SET" } )
 
-	# Next Track / Next Dir (if channel 1 engaged)
+	# Next Track 
 	buttonfunc.append( {
 	   "channel0_lo": 460
 	 , "channel0_hi": 490
@@ -145,7 +139,7 @@ def main():
 	 , "channel1_hi": 300
 	 , "wait"       : False
 	 , "delay"      : True
-	 , "zmq_path"   : "\player\next"
+	 , "zmq_path"   : "/player/next"
 	 , "zmq_cmd"    : "SET" } )
 
 	# Next Track / Next Dir (if channel 1 engaged)
@@ -156,7 +150,66 @@ def main():
 	 , "channel1_hi": 1100
 	 , "wait"       : False
 	 , "delay"      : True
-	 , "zmq_path"   : "\player\nextdir"
+	 , "zmq_path"   : "/player/nextdir"
+	 , "zmq_cmd"    : "SET" } )
+
+	# Prev Track
+	buttonfunc.append( {
+	   "channel0_lo": 560
+	 , "channel0_hi": 580
+	 , "channel1_lo": 0
+	 , "channel1_hi": 300
+	 , "wait"       : False
+	 , "delay"      : True
+	 , "zmq_path"   : "/player/prev"
+	 , "zmq_cmd"    : "SET" } )
+
+	# Prev Track / Prev Dir (if channel 1 engaged)
+	buttonfunc.append( {
+	   "channel0_lo": 560
+	 , "channel0_hi": 580
+	 , "channel1_lo": 301
+	 , "channel1_hi": 1100
+	 , "wait"       : False
+	 , "delay"      : True
+	 , "zmq_path"   : "/player/prevdir"
+	 , "zmq_cmd"    : "SET" } )
+
+	# Big trianlge (Shuffle)
+	buttonfunc.append( {
+	   "channel0_lo": 640
+	 , "channel0_hi": 670
+	 , "wait"       : True
+	 , "delay"      : None
+	 , "zmq_path"   : "/player/random"
+	 , "zmq_cmd"    : "SET" } )
+
+	# Round button with dot (ATT)
+	buttonfunc.append( {
+	   "channel0_lo": 740
+	 , "channel0_hi": 770
+	 , "wait"       : True
+	 , "delay"      : None
+	 , "zmq_path"   : "/volume/att"
+	 , "zmq_cmd"    : "SET" } )
+
+	# Source
+	buttonfunc.append( {
+	   "channel0_lo": 890
+	 , "channel0_hi": 910
+	 , "wait"       : True
+	 , "delay"      : None
+	 , "zmq_path"   : "/source/next"
+	 , "zmq_cmd"    : "SET" } )
+
+	# Round smooth button (OFF)
+	buttonfunc.append( {
+	   "channel0_lo": 1050
+	 , "channel0_hi": 110
+	 , "wait"       : True
+	 , "delay"      : None
+	 , "long_press" : 3
+	 , "zmq_path"   : "/source/next"
 	 , "zmq_cmd"    : "SET" } )
 
 	 
@@ -198,6 +251,7 @@ def main():
 	
 	def handle_button_press( button_spec ):
 		if 'zmq_path' and 'zmq_cmd' in button_spec:
+
 			zmq_send(button_spec['zmq_path'],button_spec['zmq_cmd'])
 			if button_spec['delay']:
 				button_down_delay()
@@ -234,46 +288,46 @@ def main():
 					and buttonfunc[5]['channel1_lo'] <= value_1 <= buttonfunc[5]['channel1_hi']) ):
 			handle_button_press(buttonfunc[5])
 			
-#		elif BUTTON05_LO <= value_0 <= BUTTON05_HI:
-#			if value_1 < 300:
-#				button_press('SEEK_NEXT')
-#			else:
-#				button_press('DIR_NEXT')
-#			#Wait a little...
-#			button_down_delay()
+		elif (buttonfunc[6]['channel0_lo'] <= value_0 <= buttonfunc[6]['channel0_hi'] and
+				('channel1_lo' and 'channel1_hi' in buttonfunc[6]
+					and buttonfunc[6]['channel1_lo'] <= value_1 <= buttonfunc[6]['channel1_hi']) ):
+			handle_button_press(buttonfunc[6])
+
+
+		elif ( buttonfunc[7]['channel0_lo'] <= value_0 <= buttonfunc[7]['channel0_hi'] and
+				('channel1_lo' and 'channel1_hi' in buttonfunc[7]
+					and buttonfunc[7]['channel1_lo'] <= value_1 <= buttonfunc[7]['channel1_hi']) ):
+			handle_button_press(buttonfunc[7])
+		
+		elif buttonfunc[8]['channel0_lo'] <= value_0 <= buttonfunc[8]['channel0_hi']:
+			handle_button_press(buttonfunc[8])
+
+		elif buttonfunc[9]['channel0_lo'] <= value_0 <= buttonfunc[9]['channel0_hi']:
+			handle_button_press(buttonfunc[9])
+
+		elif buttonfunc[10]['channel0_lo'] <= value_0 <= buttonfunc[10]['channel0_hi']:
+			handle_button_press(buttonfunc[10])
+
+		elif buttonfunc[11]['channel0_lo'] <= value_0 <= buttonfunc[11]['channel0_hi']:
+			if 'long_press' in button_spec:
+				printer("Waiting for button to be pressed long enough")
+				time.sleep(button_spec['long_press'])
+				value_0 = adc.read_adc(0, gain=GAIN)
+				
+				if buttonfunc[11]['channel0_lo'] <= value_0 <= buttonfunc[11]['channel0_hi']:
+					handle_button_press(buttonfunc[11])
+				else:
+					printer("Not pressed long enough, not shutting down")
 			
-		elif BUTTON06_LO <= value_0 <= BUTTON06_HI:
-			if value_1 < 300:
-				button_press('SEEK_PREV')
-			else:
-				button_press('DIR_PREV')
-			#Wait a little...
-			button_down_delay()
-
-		elif BUTTON07_LO <= value_0 <= BUTTON07_HI:
-			button_press('SHUFFLE')
-			#Wait until button is released
-			button_down_wait()
-
-		elif BUTTON08_LO <= value_0 <= BUTTON08_HI:
-			button_press('ATT')
-			#Wait until button is released
-			button_down_wait()
-
-		elif BUTTON09_LO <= value_0 <= BUTTON09_HI:
-			button_press('SOURCE')
-			#Wait until button is released
-			button_down_wait()
-
-		elif BUTTON10_LO <= value_0 <= BUTTON10_HI:
-			printer("Waiting for button to be pressed long enough")
-			time.sleep(3)
-			value_0 = adc.read_adc(0, gain=GAIN)
-			if BUTTON10_LO <= value_0 <= BUTTON10_HI:
-				printer("Long press, really shutting down...")
-				button_press('OFF')
-			else:
-				printer("Not pressed long enough, not shutting down")
+#		elif BUTTON10_LO <= value_0 <= BUTTON10_HI:	
+#			printer("Waiting for button to be pressed long enough")
+#			time.sleep(3)
+#			value_0 = adc.read_adc(0, gain=GAIN)
+#			if BUTTON10_LO <= value_0 <= BUTTON10_HI:
+#				printer("Long press, really shutting down...")
+#				button_press('OFF')
+#			else:
+#				printer("Not pressed long enough, not shutting down")
 			
 		time.sleep(0.1)
 
