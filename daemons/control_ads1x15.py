75c8cb06d6fd5e17513478da9ad120c7b47e0f95
Sjoerd82 <srg.venema@gmail.com>
2018-03-11 16:38:46 +0100

    1638

--- daemons/control_ads1x15.py
+++ daemons/control_ads1x15.py
@@ -306,15 +306,12 @@ def main():
 			handle_button_press(buttonfunc[9])
 
 		elif buttonfunc[10]['channel0_lo'] <= value_0 <= buttonfunc[10]['channel0_hi']:
-			handle_button_press(buttonfunc[10])
-
-		elif buttonfunc[11]['channel0_lo'] <= value_0 <= buttonfunc[11]['channel0_hi']:
 			if 'long_press' in button_spec:
 				printer("Waiting for button to be pressed long enough")
 				time.sleep(button_spec['long_press'])
 				value_0 = adc.read_adc(0, gain=GAIN)
 				
-				if buttonfunc[11]['channel0_lo'] <= value_0 <= buttonfunc[11]['channel0_hi']:
+				if buttonfunc[10]['channel0_lo'] <= value_0 <= buttonfunc[10]['channel0_hi']:
 					handle_button_press(buttonfunc[11])
 				else:
 					printer("Not pressed long enough, not shutting down")
