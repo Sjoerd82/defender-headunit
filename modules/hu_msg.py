aa5dc0b27c6bfa3c21122342b6590dc1b224c227
Sjoerd82 <srg.venema@gmail.com>
2018-03-21 19:33:02 +0100

    1933

--- modules/hu_msg.py
+++ modules/hu_msg.py
@@ -114,7 +114,7 @@ class MessageController():
 		#self.server.bind(server_address)
 		self.server.bind('tcp://*:5555')
 		time.sleep(1)	# still needed when polling?
-
+		self.server.connect(address)
 
 		self.is_server = True
 		
@@ -138,6 +138,7 @@ class MessageController():
 		
 		# also create a publisher, but don't bind
 		self.server = self.context.socket(zmq.PUB)
+		self.server.connect(address)
 		
 		# WE MUST NOT CALL CREATE_SERVER ANYMORE!
 		self.is_server = False
