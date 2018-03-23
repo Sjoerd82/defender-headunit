7e8cc491568b5224d14482665f4d7be6ab1982e8
Sjoerd82 <srg.venema@gmail.com>
2018-03-23 19:56:41 +0100

    1956

--- modules/hu_msg.py
+++ modules/hu_msg.py
@@ -120,10 +120,12 @@ class MqPubSubFwdController:
 		for topic in topics:
 			self.subscriber.setsockopt (zmq.SUBSCRIBE, topic)
 		
-	#	self.reply_subscriber = self.context.socket (zmq.SUB)
-	#	self.reply_subscriber.connect("tcp://{0}:{1}".format(self.address, self.port_sub))
+		# TODO; FOR SOME REASON WE NEED TO DEFINE IT HERE..
+		# .. DEFINING IT LATER, IN PUBLISH_COMMAND() DOESN'T WORK !?
+		self.reply_subscriber = self.context.socket (zmq.SUB)
+		self.reply_subscriber.connect("tcp://{0}:{1}".format(self.address, self.port_sub))
 		#self.poller.register(self.reply_subscriber, zmq.POLLIN)
-	#	self.reply_subscriber.setsockopt (zmq.SUBSCRIBE, '/bladiebla')
+		self.reply_subscriber.setsockopt (zmq.SUBSCRIBE, '/bladiebla')
 		
 	def publish_command(self, path, command, arguments=None, wait_for_reply=False, timeout=5000, response_path=None):
 		"""
@@ -160,14 +162,14 @@ class MqPubSubFwdController:
 		if wait_for_reply:
 			print "DEBUG: SETUP WAIT_FOR_REPLY; TOPIC={0}".format(response_path)
 			# create a subscription socket, listening to the response path
-			reply_subscriber = self.context.socket (zmq.SUB)
-			reply_subscriber.connect("tcp://{0}:{1}".format(self.address, self.port_sub))
-			time.sleep(1)
+	#		reply_subscriber = self.context.socket (zmq.SUB)
+	#		reply_subscriber.connect("tcp://{0}:{1}".format(self.address, self.port_sub))
+	#		time.sleep(1)
 			# setup a temporary poller for the new socket
 	#		reply_poller = zmq.Poller()
 	#		reply_poller.register(reply_subscriber, zmq.POLLIN)
-			self.poller.register(reply_subscriber, zmq.POLLIN)
-			reply_subscriber.setsockopt(zmq.SUBSCRIBE,response_path)
+			self.poller.register(self.reply_subscriber, zmq.POLLIN)
+	#		reply_subscriber.setsockopt(zmq.SUBSCRIBE,response_path)
 		
 		print "DEBUG: SENDING MESSAGE: {0}".format(message)
 		retval = self.__send(message)
@@ -183,19 +185,19 @@ class MqPubSubFwdController:
 			parsed_response = None
 	#		events = dict(reply_poller.poll()) #timeout
 			events = dict(self.poller.poll())
-			self.poller.unregister(reply_subscriber)
+			self.poller.unregister(self.reply_subscriber)
 			#except zmq.ZMQError:
 				# No Message Available
 			#	return None
 	#		if self.subscriber in events:
-			if reply_subscriber in events:
+			if self.reply_subscriber in events:
 				print "OHYEAHBABY! AGAIN"
 			
 			if events:
 				print "DEBUG: YES!"
 				# todo: have a look at what's returned?
 				# read response from the server
-				response = reply_subscriber.recv()
+				response = self.reply_subscriber.recv()
 				#parsed_response = parse_message(response)
 			else:
 				print "DEBUG: NOPE"
