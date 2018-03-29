a810c3bcc2a8b829ec227f0bc7d9c45c501418a7
Sjoerd82 <srg.venema@gmail.com>
2018-03-29 16:49:09 +0200

    1649

--- modules/source_plugin.py
+++ modules/source_plugin.py
@@ -16,6 +16,10 @@ class SourcePlugin(object):
 #		super(SourcePlugin, self).__init__(logger, name, displayname)
 #		super(SourcePlugin, self).__init__()
 		print('__INIT__ BASESOURCECLASS 1')
+		
+		print logging.Logger.manager.loggerDict.keys() 
+		
+		#self.logger=logging.getLogger('srcctrl')
 		self.logger=logging.getLogger('srcctrl')
 		printer('__INIT__ BASESOURCECLASS 2')
 
