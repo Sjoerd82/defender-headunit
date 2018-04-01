723826524092a34ac1ff04ea0a593d8ddffbe94c
Sjoerd82 <srg.venema@gmail.com>
2018-04-01 11:30:49 +0200

    1130

--- modules/hu_mpd.py
+++ modules/hu_mpd.py
@@ -258,22 +258,12 @@ class MpdController(object):
 		return state
 		
 	def track(self):
-		#mpd_track = self.mpdc.currentsong()
-		#track = {}
-		#return track
-		# ORIG:
 		self.__test_conn()
-			
-		#self.mpdc.command_list_ok_begin()
 		results = self.mpdc.currentsong()
-		#results = self.mpdc.command_list_end()
 		self.__return_to_idle()
-		print results
 		# print results[0]
 		#{'album': 'Exodus', 'composer': 'Andy Hunter/Tedd T.', 'title': 'Go', 'track': '1', 'duration': '411.480', 'artist': 'Andy Hunter', 'pos': '0', 'last-modified': '2013-10-12T15:53:13Z', 'albumartist': 'Andy Hunter', 'file': 'PIHU_SMB/music/electric/Andy Hunter/Andy Hunter - 2002 - Exodus/01 - Andy Hunter - Go.mp3', 'time': '411', 'date': '2002', 'genre': 'Electronic/Dance', 'id': '44365'}
-		
-		#return self.mpdc.currentsong()
-		return results[0]
+		return results
 	
 	"""
 	def stats(self):
