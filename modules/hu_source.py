1e457b2cd8c3198f0b804ab4c8e3940f1d8deb7b
Sjoerd82 <srg.venema@gmail.com>
2018-04-04 19:39:08 +0200

    1939

--- modules/hu_source.py
+++ modules/hu_source.py
@@ -167,8 +167,8 @@ class SourceController(object):
 				self.__printer('Plugin {0} failed (on_init); cannot load plugin'.format(plugin.name))
 			else:			
 				self.source_manager.activatePluginByName(plugin.name)
-				config = plugin.plugin_object.configuration(plugin.name)	# Get config
-				isAdded = self.add(config)									# Add
+				config = plugin.plugin_object.configuration()			# Get config
+				isAdded = self.add(config)								# Add
 				if isAdded:
 					indexAdded = self.index('name',config['name'])
 					try:
