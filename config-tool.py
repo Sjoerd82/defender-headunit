ddb50309558668cadd5c4cc89be1a3ef0b325492
Sjoerd82 <srg.venema@gmail.com>
2018-04-26 22:01:20 +0200

    2201

--- config-tool.py
+++ config-tool.py
@@ -294,7 +294,7 @@ def main():
 			write_config_ecs( configuration['system_configuration']['ecasound_ecs'] )
 			
 			if 'ecasound' in configuration and 'chainsetup' in configuration['ecasound']:
-				ecs_symlink = os.path.join(configuration['system_configuration']['ecasound_ecs']['location'],configuration['ecasound']['chainsetup'])
+				ecs_symlink = os.path.join(configuration['system_configuration']['ecasound_ecs']['location'],configuration['ecasound']['chainsetup']+'.ecs')
 				ecs_symlink_target = os.path.join(configuration['system_configuration']['ecasound_ecs']['location'],'active.ecs')
 				printer("Creating symlink: {0} to {1}".format(ecs_symlink,ecs_symlink_target))
 				os.symlink(ecs_symlink, ecs_symlink_target)
