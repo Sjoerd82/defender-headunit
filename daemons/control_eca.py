4ec3c273a84cdc4ef2e5e584dc7ff2ac70e4f2af
Sjoerd82 <srg.venema@gmail.com>
2018-05-08 18:20:10 +0200

    1820

--- daemons/control_eca.py
+++ daemons/control_eca.py
@@ -344,9 +344,9 @@ def get_data(ret,returndata=False,eventpath=None):
 		data['retval'] = 200
 		data['payload'] = ret
 
-	if eventpath == '/events/volume/changed' or 
-	   eventpath == '/events/volume/att' or
-	   eventpath == '/events/volume/mute'
+	if ( eventpath == '/events/volume/changed' or 
+	     eventpath == '/events/volume/att' or
+	     eventpath == '/events/volume/mute' )
 		data['payload'] = curr_source
 		messaging.publish_command(eventpath,'DATA',data)
 			
