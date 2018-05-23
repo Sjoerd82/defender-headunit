1a485648af4e0da4a9239aac6d88fb69fd8df1ad
Sjoerd82 <srg.venema@gmail.com>
2018-05-23 21:46:24 +0200

    2146

--- modules/hu_msg.py
+++ modules/hu_msg.py
@@ -145,9 +145,9 @@ class MqPubSubFwdController(object):
 		#printer(colorize("Sending MQ message: {0}".format(message),'dark_gray'),level=LL_DEBUG)
 	#	printer("Sending MQ message: {0}".format(message), level=LL_DEBUG, tag='MQ')
 	#	printer("Sending MQ message: {0}".format(message))
-	#	print("Sending MQ message: {0}".format(message))
+		print("Sending MQ message: {0}".format(message))
 		self.publisher.send(message)
-		time.sleep(1)	# required??? don't think so.. TODO: Remove, once we seen everything works..
+		#time.sleep(1)	# required??? don't think so.. TODO: Remove, once we seen everything works..
 		return True
 
 	def __recv(self):
