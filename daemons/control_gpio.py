6ae744394c3629e13527ad6ffc50ba04e24b2a19
Sjoerd82 <srg.venema@gmail.com>
2018-06-03 17:49:09 +0200

    1749

--- daemons/control_gpio.py
+++ daemons/control_gpio.py
@@ -354,7 +354,7 @@ def setup():
 	global modes
 	gpio = GpioController(cfg_gpio,cb_gpio_function)
 	ret = gpio.get_modes()
-	modes = ret.copy()
+	modes = copy.copy(ret)
 	gpio.set_cb_mode_change(cb_mode_change)
 	
 	printer('Initialized [OK]')
