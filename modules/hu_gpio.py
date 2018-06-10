529573ccc2c39a3994713250fba3c3825056ea80
Sjoerd82 <srg.venema@gmail.com>
2018-06-10 18:18:51 +0200

    1818

--- modules/hu_gpio.py
+++ modules/hu_gpio.py
@@ -79,15 +79,16 @@ class GpioController(object):
 	
 	def get_modes(self):
 		""" Returns Mode-structure containing all modes and states of all sets. """
-		master_modes_list = Modes()
-		for mode_set_id,mode_set in self.mode_sets.iteritems():
-			#print mode_set_id
-			#print mode_set
-			if mode_set_id != 'active_modes':
-				master_modes_list.extend(mode_set['mode_list'])
-				#print mode_set['mode_list']
-			
-		return copy.deepcopy(master_modes_list)		# list of dicts, requires deepcopy() instead of copy()
+		if self.mode_sets is not None:
+			master_modes_list = Modes()
+			for mode_set_id,mode_set in self.mode_sets.iteritems():
+				#print mode_set_id
+				#print mode_set
+				if mode_set_id != 'active_modes':
+					master_modes_list.extend(mode_set['mode_list'])
+					#print mode_set['mode_list']
+				
+			return copy.deepcopy(master_modes_list)		# list of dicts, requires deepcopy() instead of copy()
 	
 	# ********************************************************************************
 	# GPIO helpers
@@ -114,14 +115,11 @@ class GpioController(object):
 		# loop through all possible functions for given pin
 		# examine if func meets all requirements (only one check needed for encoders: mode)
 		self.__update_active_modes()
-		print "X1 -- {0}".format(self.mode_sets['active_modes'])
 		for func_cfg in self.pins_config[pin]['functions']:
 			# check mode #TODO!! TODO!! add mode here!!
 			if 'mode' in func_cfg and func_cfg['mode'] not in self.mode_sets['active_modes']:
-				print "X3"
 				pass # these are not the mode you're looking for
 			else:
-				print "X4"
 				#if 'encoder' in func_cfg:
 				return func_cfg
 				
@@ -151,7 +149,6 @@ class GpioController(object):
 		self.__printer('[MODE] Reset to: "{0}"'.format(self.mode_sets[mode_set_id]['base_mode']))
 		
 		self.__update_active_modes()
-		print "X0 reset -- {0}".format(self.mode_sets['active_modes'])	
 		
 		# call that other callback
 		master_modes_list = self.get_modes()
