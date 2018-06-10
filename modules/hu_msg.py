855ec80194035a4de314b4bc490f4c8700fbf4e4
Sjoerd82 <srg.venema@gmail.com>
2018-06-10 16:03:44 +0200

    1603

--- modules/hu_msg.py
+++ modules/hu_msg.py
@@ -378,12 +378,15 @@ class MqPubSubFwdController(object):
 	#********************************************************************************
 
 	# EXPERIMENTAL
-	def poll_and_execute(self,timeout):
+	def poll_and_execute(self,timeout,ignore_own_message=True):
 		parsed_msg = self.poll(timeout=500, parse=True)	#Timeout: None=Blocking
 		if parsed_msg:
-			ret = self.execute_mq(parsed_msg['path'], parsed_msg['cmd'], args=parsed_msg['args'], data=parsed_msg['data'])
-			if parsed_msg['resp_path'] and ret is not None:
-				self.publish_command(parsed_msg['resp_path'],'DATA',ret)
+			if parsed_msg['origin'] == self.origin and ignore_own_message:
+				pass
+			else:
+				ret = self.execute_mq(parsed_msg['path'], parsed_msg['cmd'], args=parsed_msg['args'], data=parsed_msg['data'])
+				if parsed_msg['resp_path'] and ret is not None:
+					self.publish_command(parsed_msg['resp_path'],'DATA',ret)
 				
 		return True
 	
