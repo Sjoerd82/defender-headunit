bbf5b2c57af6237310278e034932c41aadd54602
Sjoerd82 <srg.venema@gmail.com>
2018-06-10 11:23:55 +0200

    1123

--- daemons/control_gpio.py
+++ daemons/control_gpio.py
@@ -366,7 +366,7 @@ def setup():
 	global cfg_gpio
 
 	# main
-	cfg_main, cfg_zmq, cfg_daemon, cfg_gpio = load_cfg = load_cfg(args.config, ['main','zmq','daemon','gpio'], args.port_subscriber, args.port_subscriber)
+	cfg_main, cfg_zmq, cfg_daemon, cfg_gpio = load_cfg(args.config, ['main','zmq','daemon','gpio'], args.port_subscriber, args.port_subscriber)
 		
 	#cfg_main = load_cfg_main()
 	if cfg_main is None:
