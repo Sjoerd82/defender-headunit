70757b1f6d48db984c40ca6d3dc65f3b80d04d6f
Sjoerd82 <srg.venema@gmail.com>
2018-06-11 21:37:42 +0200

    2137

--- modules/hu_commands.py
+++ modules/hu_commands.py
@@ -311,7 +311,7 @@ class Commands(object):
 			]
 
 		for command in self.function_mq_map:
-			self.commands.append(command['name'])
+			self.command_list.append(command['name'])
 		
 	def get_command(self, command):
 		ix = self.command_list.index(code)
