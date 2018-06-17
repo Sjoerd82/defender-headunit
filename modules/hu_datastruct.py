03d7bbc1d5ebaf3a1e63c0813ce8dc6ef56b5e79
Sjoerd82 <srg.venema@gmail.com>
2018-06-17 17:26:23 +0200

    1726

--- modules/hu_datastruct.py
+++ modules/hu_datastruct.py
@@ -62,7 +62,7 @@ class Modeset(list):
 		#if len(self) > 0:
 		#print "-- __contains: -- {0} {1}".format(type(self[0]),self[0])
 		for key, value in self:
-			print key
+			print "self={0} key={1}".format(self,key)
 			if key == 'mode':
 				print "{0}, {1}".format(value,str(item))
 				if value == str(item):
