e3c06c5b6f5e4fb562b00da92ea6cc9bdac50b0d
Sjoerd82 <srg.venema@gmail.com>
2018-06-23 14:33:28 +0200

    1433

--- daemons/control_eca.py
+++ daemons/control_eca.py
@@ -28,7 +28,6 @@ from Queue import Queue		# queuing
 #sys.path.append('../modules')
 sys.path.append('/mnt/PIHU_APP/defender-headunit/modules')
 from hu_utils import *
-from hu_datastruct import Modes
 from hu_gpio import GpioController
 from hu_msg import MqPubSubFwdController
 
